4184d960c6dd433b031fc5af148b28e6e1d75d23
Thakhani Tharage <thakhani@deltasolutions.co.za>
2020-04-17 12:22:47 +0200

    Remove unnecessary folder structure

--- src/app/adminLayout/admin-layout/admin-layout.module.ts
+++ src/app/adminLayout/admin-layout/admin-layout.module.ts
@@ -1,32 +1,34 @@
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {NgModule} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import {RouterModule} from '@angular/router';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
-import { AdminLayoutRoutes } from './admin-layout.routing';
-import {AdminComponent} from '../../pages/adminFolder/admin/admin.component';
-import {TeamsComponent} from '../../pages/teamsFolder/teams/teams.component';
-import {ArchitectureComponent} from '../../pages/architectureFolder/architecture/architecture.component';
-import {AboutUsComponent} from '../../pages/aboutUsFolder/about-us/about-us.component';
-import {ProjectsComponent} from '../../pages/projectsFolder/projects/projects.component';
-import {HomeComponent} from '../../pages/homeFolder/home/home.component';
-import {InfrastructureComponent} from '../../pages/infrastructureFolder/infrastructure/infrastructure.component';
-import {PageNotFoundComponent} from '../../pages/pageNotFoundFolder/page-not-found/page-not-found.component';
-import {ContactUsComponent} from '../../pages/contactUsFolder/contact-us/contact-us.component';
-import {DashboardComponent} from '../../pages/dashboardFolder/dashboard/dashboard.component';
+import {AdminLayoutRoutes} from './admin-layout.routing';
+import {AdminComponent} from '../../pages/admin/admin.component';
+import {TeamsComponent} from '../../pages/teams/teams.component';
+import {ArchitectureComponent} from '../../pages/architecture/architecture.component';
+import {AboutUsComponent} from '../../pages/about-us/about-us.component';
+import {ProjectsComponent} from '../../pages/projects/projects.component';
+import {HomeComponent} from '../../pages/home/home.component';
+import {InfrastructureComponent} from '../../pages/infrastructure/infrastructure.component';
+import {PageNotFoundComponent} from '../../pages/page-not-found/page-not-found.component';
+import {ContactUsComponent} from '../../pages/contact-us/contact-us.component';
+import {DashboardComponent} from '../../pages/dashboard/dashboard.component';
 import {MatTabsModule} from '@angular/material/tabs';
 import {FlexModule} from '@angular/flex-layout';
+import {MatCardModule} from "@angular/material/card";
 
 @NgModule({
-    imports: [
-        CommonModule,
-        RouterModule.forChild(AdminLayoutRoutes),
-        FormsModule,
-        HttpClientModule,
-        MatTabsModule,
-        FlexModule,
-    ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(AdminLayoutRoutes),
+    FormsModule,
+    HttpClientModule,
+    MatTabsModule,
+    FlexModule,
+    MatCardModule,
+  ],
   declarations: [
     DashboardComponent,
     AboutUsComponent,
