8d51e8e2482ec234606c822cf4acfaf802fea65c
TXW <tlangelani.maswanganye@rmb.co.za>
2020-04-21 11:19:09 +0200

    Service call

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup} from '@angular/forms';
 import {map, startWith} from 'rxjs/operators';
 import {Observable} from 'rxjs';
+import {AdminService} from './admin.service';
 
 @Component({
   selector: 'app-admin',
@@ -17,49 +18,53 @@ export class AdminComponent implements OnInit {
   });
 
   fullNameList  = [
-    {id: 1, name: 'Tlangelani Maswanganye'},
+    /* {id: 1, name: 'Tlangelani Maswanganye'},
     {id: 2, name: 'Happy Smith'},
-    {id: 3, name: 'Jerry Zoom'}
+    {id: 3, name: 'Jerry Zoom'}*/
   ];
 
   teamList = [
-    {id: 1, name: 'FinTech Developers'},
+    /*{id: 1, name: 'FinTech Developers'},
     {id: 2, name: 'FinTech Testers'},
     {id: 3, name: 'App Support'},
     {id: 4, name: 'Production Support'},
-    {id: 5, name: 'Oracle'}
+    {id: 5, name: 'Oracle'}*/
     ];
 
   statusList = [
-    {id: 1, name: 'Active'},
-    {id: 2, name: 'Inactive'}
+   /* {id: 1, name: 'Active'},
+    {id: 2, name: 'Inactive'} */
     ];
 
   filteredFullName: Observable<any[]>;
   filteredTeams: Observable<any[]>;
   filteredStatus: Observable<any[]>;
 
+  constructor(private adminService: AdminService) {
+  }
+
   ngOnInit() {
+    this.adminService.fetchData();
     this.filteredFullName = this.employees.controls.fullName.valueChanges
       .pipe(
         startWith(''),
-        map(fullName => this.filterName(fullName))
+        map(fullName => this.adminService.filterName(fullName))
       );
 
     this.filteredTeams = this.employees.controls.teams.valueChanges
       .pipe(
         startWith(''),
-        map(teams => this.filterTeams(teams))
+        map(teams => this.adminService.filterTeams(teams))
       );
 
     this.filteredStatus = this.employees.controls.status.valueChanges
       .pipe(
         startWith(''),
-        map(status => this.filterStatus(status))
+        map(status => this.adminService.filterStatus(status))
       );
   }
 
-  private filterName(fullName: string) {
+  /*private filterName(fullName: string) {
     return this.fullNameList.filter(option => new RegExp(fullName).test(option.name));
   }
 
@@ -69,7 +74,7 @@ export class AdminComponent implements OnInit {
 
   private filterStatus(status: string) {
     return this.statusList.filter(option => new RegExp(status).test(option.name));
-  }
+  } */
 
   displayFunction(subject) {
     return subject ? subject.name : undefined;
--- src/app/pages/admin/admin.service.ts
+++ src/app/pages/admin/admin.service.ts
@@ -1,9 +1,57 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
 
-  constructor() { }
+  baseUrl = '../assets/data/';
+  fullNameList: any[] = [];
+  teamList: any[] = [];
+  statusList: any[] = [];
+
+  constructor(private httpClient: HttpClient) { }
+
+
+  private getEmployees(): Observable<any> {
+    return this.httpClient.get(this.baseUrl + 'employees.json');
+  }
+
+  private getTeams(): Observable<any> {
+    return this.httpClient.get(this.baseUrl + 'teams.json');
+  }
+
+  private getStatus(): Observable<any> {
+    return this.httpClient.get(this.baseUrl + 'status.json');
+  }
+
+  fetchData() {
+
+    this.getEmployees().subscribe(data => {
+      this.fullNameList = [data];
+    });
+
+    this.getTeams().subscribe(data => {
+      this.teamList = [data];
+    });
+
+    this.getStatus().subscribe(data => {
+      this.statusList = [data];
+    });
+  }
+
+  public filterName(fullName: string) {
+    return this.fullNameList.filter(option => new RegExp(fullName).test(option.name));
+  }
+
+  public filterTeams(teams: string) {
+    return this.teamList.filter(option =>  new RegExp(teams).test(option.name));
+  }
+
+  public filterStatus(status: string) {
+    return this.statusList.filter(option => new RegExp(status).test(option.name));
+  }
+
 }
