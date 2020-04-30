aa96a378a330b826ef794007d792da16b2e82da5
TXW <tlangelani.maswanganye@rmb.co.za>
2020-04-30 14:24:17 +0200

    display Data with paginator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -64,6 +64,7 @@ import {MatCardModule} from '@angular/material/card';
     { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptorService, multi: true },
     fakeBackendProvider
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
