30e9f4b571b2fc93b10cae64460a4728e90afe3f
TXW <tlangelani.maswanganye@rmb.co.za>
2020-05-06 11:19:59 +0200

    I am changing the tables for all tabs to use one data table but have different data sources.

--- src/app/pages/admin/data-table/data-table.component.ts
+++ src/app/pages/admin/data-table/data-table.component.ts
@@ -3,6 +3,7 @@ import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTable } from '@angular/material/table';
 import { DataTableDataSource, DataTableItem } from './data-table-datasource';
+import {ApplicationsTableDataSource} from '../applications/applications-table/applications-table-datasource';
 
 @Component({
   selector: 'app-data-table',
@@ -10,33 +11,52 @@ import { DataTableDataSource, DataTableItem } from './data-table-datasource';
   styleUrls: ['./data-table.component.css']
 })
 export class DataTableComponent implements AfterViewInit, OnInit, OnChanges {
-  @Input() filterValue: string;
+  @Input() usersFullNameFilterValue: string;
+  @Input() applicationsFilterValue: string;
+  @Input() mainTabIndex: number;
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
   @ViewChild(MatTable) table: MatTable<DataTableItem>;
-  employeesDataSource: DataTableDataSource;
+  usersFullNameDataSource: DataTableDataSource;
+  applicationsDataSource: ApplicationsTableDataSource;
 
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
   displayedColumns = ['id', 'name'];
 
   ngOnInit() {
-    this.employeesDataSource = new DataTableDataSource();
+    this.usersFullNameDataSource = new DataTableDataSource();
+    this.applicationsDataSource = new ApplicationsTableDataSource();
   }
 
   ngAfterViewInit() {
-    this.employeesDataSource.sort = this.sort;
-    this.employeesDataSource.paginator = this.paginator;
-    this.table.dataSource = this.employeesDataSource;
+    if (this.mainTabIndex === 0) {
+      this.usersFullNameDataSource.sort = this.sort;
+      this.usersFullNameDataSource.paginator = this.paginator;
+      this.table.dataSource = this.usersFullNameDataSource;
+    } else if (this.mainTabIndex === 1) {
+      this.applicationsDataSource.sort = this.sort;
+      this.applicationsDataSource.paginator = this.paginator;
+      this.table.dataSource = this.applicationsDataSource;
+    }
   }
   ngOnChanges(changes: SimpleChanges): void {
     this.doFilter();
   }
 
   public doFilter() {
-    // console.log('Value in the table: ' + this.filterValue);
-    if (this.filterValue.length > 0) {
-      // console.log('Value in the table inside loop: ' + this.filterValue);
-      this.table.dataSource = this.employeesDataSource.data.filter(option => new RegExp(this.filterValue, 'gi').test(option.name));
+    if (this.mainTabIndex === 0) {
+      if (this.usersFullNameFilterValue.length > 0) {
+        // console.log('Value in the table inside loop: ' + this.usersFullNameFilterValue);
+        this.table.dataSource = this.usersFullNameDataSource.data.filter(option =>
+          new RegExp(this.usersFullNameFilterValue, 'gi').test(option.name));
+      }
+    }else if (this.mainTabIndex === 1) {
+      if (this.applicationsFilterValue.length > 0) {
+        // console.log('Value in the table inside loop: ' + this.usersFullNameFilterValue);
+        this.table.dataSource = this.applicationsDataSource.data.filter(option =>
+          new RegExp(this.applicationsFilterValue, 'gi').test(option.name));
+      }
     }
+
   }
 }
