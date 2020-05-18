e0cfe1c5ac56122a8388dc4dca81953d212d1371
TXW <tlangelani.maswanganye@rmb.co.za>
2020-05-18 20:27:54 +0200

    I have added the icons for both the edit and delete buttons

--- src/app/pages/admin/employees/employees.component.ts
+++ src/app/pages/admin/employees/employees.component.ts
@@ -99,12 +99,14 @@ export class EmployeesComponent implements OnInit, OnChanges {
   private filterTeams(value: string) {
       this.teamList = this.dataSource.data;
       if (value.length >= 2) {
-        this.dataSource.data.filter((name, key) => {
-          if (name.name === this.employees.controls.fullName.value) {
-            this.nameExist = true;
-            console.log('What is the value: ' + this.employees.controls.fullName.value);
-          }
-        });
+        if (this.action === 'Add') {
+          this.dataSource.data.filter((name, key) => {
+            if (name.name === this.employees.controls.fullName.value) {
+              this.nameExist = true;
+              console.log('What is the value: ' + this.employees.controls.fullName.value);
+            }
+          });
+        }
         return this.teamList.filter(option =>  new RegExp(value, 'gi').test(option.team));
       } else {
         this.nameExist = false;
