96aa9d17c0f99cf8a8eff2b276bdc68b440e0bb5
TXW <tlangelani.maswanganye@rmb.co.za>
2020-05-22 07:59:08 +0200

    I have added a registration form without validations

--- src/app/authentication/register-user/register-user.component.ts
+++ src/app/authentication/register-user/register-user.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import validate = WebAssembly.validate;
+import {Router} from '@angular/router';
+import {AuthenticationService} from '../services/authentication.service';
+import {UsersService} from '../services/users.service';
+import {AlertService} from '../../helpers/service/alert.service';
+import {first} from 'rxjs/operators';
 
 @Component({
   selector: 'app-register-user',
@@ -7,9 +14,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterUserComponent implements OnInit {
 
-  constructor() { }
+  loading = false;
+
+  registerForm: FormGroup = new FormGroup({
+    firstName: new FormControl('', Validators.required),
+    lastName: new FormControl('', Validators.required),
+    username: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    confirmEmail: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    confirmPassword: new FormControl('', [Validators.required, Validators.minLength(6)]),
+  });
+
+  constructor(private router: Router,
+              private authenticationService: AuthenticationService,
+              private userService: UsersService,
+              private alertService: AlertService) {
+    if (this.authenticationService.currentUserValue) {
+      this.router.navigate(['/']);
+    }
+  }
 
   ngOnInit(): void {
   }
+
+  get formFields() { return this.registerForm.controls; }
+
+  onSubmit() {
+    this.alertService.clear();
+
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    this.loading = true;
+    this.userService.register(this.registerForm.value)
+      .pipe(first())
+      .subscribe(data => {
+        this.alertService.success('Registration Successful', true);
+        this.router.navigate(['/login']);
+      },
+      error => {
+        this.alertService.error(error);
+        this.loading = false;
+      });
+  }
+
 
 }
