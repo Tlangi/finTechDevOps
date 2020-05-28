b59ef26ae4eba39dd82c17fbae813a902282f9d7
TXW <tlangelani.maswanganye@rmb.co.za>
2020-05-28 12:21:38 +0200

    I have added the reset-password.component.html and its component. I have also added the form for resetting your password. This will be open after from the email

--- src/app/authentication/reset-password/reset-password.component.ts
+++ src/app/authentication/reset-password/reset-password.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import {AuthenticationService} from '../services/authentication.service';
+import {UsersService} from '../services/users.service';
+import {AlertService} from '../../helpers/service/alert.service';
+import {first} from 'rxjs/operators';
 
 @Component({
   selector: 'app-reset-password',
@@ -7,9 +13,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResetPasswordComponent implements OnInit {
 
-  constructor() { }
+  loading = false;
+  submitted = false;
+  resetPasswordForm: FormGroup;
+  constructor(private router: Router,
+              private formBuilder: FormBuilder,
+              private authenticationService: AuthenticationService,
+              private userService: UsersService,
+              private alertService: AlertService
+  ) {
+    if (this.authenticationService.currentUserValue) {
+      this.router.navigate(['/']);
+    }
+  }
 
   ngOnInit(): void {
+    this.resetPasswordForm = this.formBuilder.group({
+      password: ['', [Validators.required]],
+      confirmPassword: ['', [Validators.required]],
+    });
   }
+
+  get formFields() { return this.resetPasswordForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+    this.alertService.clear();
+
+    if (this.resetPasswordForm.invalid) {
+      return;
+    }
+
+    this.loading = true;
+    this.userService.register(this.resetPasswordForm.value)
+      .pipe(first())
+      .subscribe(data => {
+          this.alertService.success('Password successfully reset', true);
+          this.router.navigate(['/login']);
+        },
+        error => {
+          this.alertService.error(error);
+          this.loading = false;
+        });
+  }
+
 
 }
