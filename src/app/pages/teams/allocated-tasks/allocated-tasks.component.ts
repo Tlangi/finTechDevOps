d9011a1b686b9e2857c299035b770060555dc918
TXW <tlangelani.maswanganye@rmb.co.za>
2020-06-02 00:34:25 +0200

    Adding style

--- src/app/pages/teams/allocated-tasks/allocated-tasks.component.ts
+++ src/app/pages/teams/allocated-tasks/allocated-tasks.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, OnChanges, OnInit, ViewChild} from '@angular/core';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatSort} from '@angular/material/sort';
 import {MatTable} from '@angular/material/table';
@@ -6,13 +6,14 @@ import {TasksUpdateDataSource, TasksUpdateItem} from '../tasks-update/tasks-upda
 import {AuthenticationService} from '../../../authentication/services/authentication.service';
 import {FormControl, FormGroup} from '@angular/forms';
 import {DatePipe} from '@angular/common';
+import {from} from 'rxjs';
 
 @Component({
   selector: 'app-allocated-tasks',
   templateUrl: './allocated-tasks.component.html',
   styleUrls: ['./allocated-tasks.component.css']
 })
-export class AllocatedTasksComponent implements OnInit, AfterViewInit {
+export class AllocatedTasksComponent implements OnInit, OnChanges, AfterViewInit {
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
   @ViewChild(MatTable) table: MatTable<TasksUpdateItem>;
@@ -25,6 +26,8 @@ export class AllocatedTasksComponent implements OnInit, AfterViewInit {
     fromDate: new FormControl(),
     toDate: new FormControl(),
   });
+  fromDate: Date;
+  toDate: Date;
 
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
   displayedColumns = ['id', 'systemOrProject', 'taskName', 'taskDescription',
@@ -34,11 +37,6 @@ export class AllocatedTasksComponent implements OnInit, AfterViewInit {
   constructor(private authenticationService: AuthenticationService) {
     this.dataSource = new TasksUpdateDataSource();
     this.pipe = new DatePipe('en');
-    if (this.authenticationService.currentUser) {
-      this.authenticationService.currentUser.subscribe(user => {
-        this.fullName = user.firstName + ' ' + user.lastName;
-      });
-    }
   }
 
   ngOnInit() {
@@ -52,6 +50,27 @@ export class AllocatedTasksComponent implements OnInit, AfterViewInit {
         new RegExp(this.fullName, 'gi').test(option.employeeName)
       ).slice(0, 5);
     }
+  }
+
+  ngOnChanges() {
+  }
+
+  private filterDateRange() {
+    /* this.allocatedTasksFilter.controls.fromDate.valueChanges.subscribe( (date) => {
+      console.log(date);
+      this.fromDate = date;
+      console.log(this.fromDate);
+    });
+    this.allocatedTasksFilter.controls.toDate.valueChanges.subscribe( (date) => {
+      this.toDate = date;
+      console.log(this.toDate);
+    });
+    this.dataSource.data = this.dataSource.data.filter(
+      date => new Date(date.expectedReleaseDate) >= new Date(this.fromDate) &&
+        new Date(date.expectedReleaseDate) <= new Date(this.toDate)
+    );
+
+    console.log(this.dataSource.data); */
   }
 
 }
